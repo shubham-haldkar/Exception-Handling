bb82257ec7856084fa5426716182dbbce4916160
Shubham-Haldkar <shubham.haldkar@gmail.com>
2024-02-18 21:41:19 +0530

    TryWithResourcesRunner

--- src/com/siteinvokers/TryWithResourcesRunner.java
+++ src/com/siteinvokers/TryWithResourcesRunner.java
@@ -6,9 +6,12 @@ public class TryWithResourcesRunner {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		Scanner sc = new Scanner(System.in);
-	 
-
+		// this will automatically close the the scanner no need to close it
+		// there is no need of catch and finally block also, if u want u can add that
+		try (Scanner sc = new Scanner(System.in)) {
+			int[] num = { 1, 2, 3 };
+			int nm = num[4];
+		}
 		System.out.println(" Before main close");
 
 	}
