ac4c089c92d21955898f13cbad4504bcb35eeaaf
Shubham-Haldkar <shubham.haldkar@gmail.com>
2024-02-18 21:35:15 +0530

    TryWithResourcesRunner

---
+++ src/com/siteinvokers/TryWithResourcesRunner.java
@@ -0,0 +1,16 @@
+package com.siteinvokers;
+
+import java.util.Scanner;
+
+public class TryWithResourcesRunner {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+	 
+
+		System.out.println(" Before main close");
+
+	}
+
+}
